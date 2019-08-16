eb391401c16b2736f496d84da7bd11c1ecada0b4
Martin Streicher <martin.streicher@gadget.consulting>
2019-08-16 13:34:44 -0400

    Warning (#1)
    * Add `:ttl` option for `memoize` method (#11)
    
    Repeat calling original method after a specified time (in seconds).
    
    * Add `--warnings` option for RSpec (#13)
    
    * Add benchmark script (#14)
    
    Can be executed by `bundle exec ruby benchmark.rb` or `bundle exec rake benchmark`.
    
    * Repair code to avoid "instance variable not initialized" warning
    
    * Revert previous change

--- lib/memery.rb
+++ lib/memery.rb
@@ -3,26 +3,32 @@
 require "memery/version"
 
 module Memery
-  def self.included(base)
-    base.extend(ClassMethods)
-    base.include(InstanceMethods)
-  end
+  class << self
+    def included(base)
+      base.extend(ClassMethods)
+      base.include(InstanceMethods)
+    end
 
-  def self.method_visibility(klass, method_name)
-    case
-    when klass.private_method_defined?(method_name)
-      :private
-    when klass.protected_method_defined?(method_name)
-      :protected
-    when klass.public_method_defined?(method_name)
-      :public
+    def method_visibility(klass, method_name)
+      case
+      when klass.private_method_defined?(method_name)
+        :private
+      when klass.protected_method_defined?(method_name)
+        :protected
+      when klass.public_method_defined?(method_name)
+        :public
+      end
+    end
+
+    def monotonic_clock
+      Process.clock_gettime(Process::CLOCK_MONOTONIC)
     end
   end
 
   module ClassMethods
-    def memoize(method_name, condition: nil)
+    def memoize(method_name, condition: nil, ttl: nil)
       prepend_memery_module!
-      define_memoized_method!(method_name, condition: condition)
+      define_memoized_method!(method_name, condition: condition, ttl: ttl)
     end
 
     private
@@ -33,7 +39,7 @@ module Memery
       prepend @_memery_module
     end
 
-    def define_memoized_method!(method_name, condition: nil)
+    def define_memoized_method!(method_name, condition: nil, ttl: nil)
       mod_id = @_memery_module.object_id
       visibility = Memery.method_visibility(self, method_name)
       raise ArgumentError, "Method #{method_name} is not defined on #{self}" unless visibility
@@ -47,10 +53,16 @@ module Memery
           @_memery_memoized_values ||= {}
           key = "#{method_name}_#{mod_id}"
           @_memery_memoized_values[key] ||= {}
-          store = @_memery_memoized_values[key]
-          return store[args] if store&.key?(args)
+          store = @_memery_memoized_values[key] || {}
 
-          @_memery_memoized_values[key][args] = super(*args)
+          if store.key?(args) && (ttl.nil? || Memery.monotonic_clock <= store[args][:time] + ttl)
+            return store[args][:result]
+          end
+
+          super(*args).tap do |result|
+            @_memery_memoized_values[key][args] =
+              { result: result, time: Memery.monotonic_clock }
+          end
         end
 
         send(visibility, method_name)
--- memery.gemspec
+++ memery.gemspec
@@ -20,6 +20,8 @@ Gem::Specification.new do |spec|
   end
   spec.require_paths = ["lib"]
 
+  spec.add_development_dependency "benchmark-ips"
+  spec.add_development_dependency "benchmark-memory"
   spec.add_development_dependency "bundler"
   spec.add_development_dependency "coveralls"
   spec.add_development_dependency "pry"
