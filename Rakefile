7cb68b9295ed6eee1e02edc4640c61b775115593
Alexander Popov <alex.wayfer@gmail.com>
2019-08-16 17:21:21 +0300

    Add benchmark script (#14)
    Can be executed by `bundle exec ruby benchmark.rb` or `bundle exec rake benchmark`.

--- Rakefile
+++ Rakefile
@@ -6,3 +6,7 @@ require "rspec/core/rake_task"
 RSpec::Core::RakeTask.new(:spec)
 
 task default: :spec
+
+task :benchmark do
+  require_relative "./benchmark"
+end
---
+++ benchmark.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "bundler/setup"
+Bundler.setup
+
+require "benchmark"
+require "benchmark/ips"
+require "benchmark/memory"
+
+puts "```ruby"
+puts File.read(__FILE__)
+puts "```"
+puts
+puts "### Output"
+puts
+puts "```"
+
+require_relative "lib/memery"
+
+class Foo
+  class << self
+    include Memery
+
+    def base_find(char)
+      ("a".."k").find { |letter| letter == char }
+    end
+
+    memoize def find_new(char)
+      base_find(char)
+    end
+  end
+end
+
+def test_memery
+  Foo.find_new("d")
+end
+
+Benchmark.ips do |x|
+  x.report("test_memery") { test_memery }
+end
+
+Benchmark.memory do |x|
+  x.report("test_memery") { 100.times { test_memery } }
+end
+
+puts "```"
